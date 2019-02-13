f6bd8131a3ad0f1c6267df6f69679cf2537fd779
Thomas van der velde <contact@toolbarthomas.net>
2019-02-13 12:04:16 +0100

    Implements postcss plugins with external config support.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,28 @@
-module.exports = {
-  plugins: {
+const glob = require('glob');
+
+module.exports = function config(e) {
+  const plugins = {
+    'postcss-at-rules-variables': {},
+    'postcss-each': {},
     'postcss-import': {},
+    'postcss-for': {},
+    'postcss-conditionals': {},
     'postcss-nested': {},
     'postcss-preset-env': {},
     cssnano: {},
-  },
+  };
+
+  // Define the path of the optional configuration file.
+  const maps = glob.sync(`${e.file.dirname}/*.yml`);
+
+  // Include the Postcss Mapping plugin if an YAML configuration file is defined.
+  if (maps.length > 0) {
+    plugins['postcss-map'] = {
+      maps,
+    };
+  }
+
+  return {
+    plugins,
+  };
 };
